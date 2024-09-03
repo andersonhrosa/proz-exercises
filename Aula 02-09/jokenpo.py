0320a5abb7d1db3b541f134414a8ba05847e0db7
Anderson Henrique da Rosa <andersonhrosa@outlook.com>
2024-09-02 21:26:09 -0300

    app final

--- Aula 02-09/jokenpo.py
+++ Aula 02-09/jokenpo.py
@@ -4,6 +4,9 @@ import random
 
 def jokenpo():
     opcoes = ['pedra', 'papel', 'tesoura']
+    vitorias=0
+    derrotas=0
+    empates=0
     while True:
         jogador = input('Escolha pedra, papel ou tesoura (ou "q" para sair): ')
         if jogador.lower() == 'q':
@@ -15,14 +18,21 @@ def jokenpo():
         computador = random.choice(opcoes)
         print(f'O computador escolheu {computador}.')
         if jogador.lower() == computador:
+            empates+=1
             print('Empate!')
         elif jogador.lower() == 'pedra' and computador == 'tesoura':
+            vitorias+=1
             print('Você ganhou!')
         elif jogador.lower() == 'papel' and computador == 'pedra':
+            vitorias+=1
             print('Você ganhou!')
         elif jogador.lower() == 'tesoura' and computador == 'papel':
+            vitorias+=1
             print('Você ganhou!')
         else:
+            derrotas+=1
             print('Você perdeu!')
 
+    print(f'Você ganhou {vitorias} vezes, perdeu {derrotas} vezes e empatou {empates} vezes.')
+    
 jokenpo()
